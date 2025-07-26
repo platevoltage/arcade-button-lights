3af9ab9abfc3106e02e89bb8aafc80ac976d93ed
Garrett Corbin <jgarrettcorbin@gmail.com>
2025-07-25 23:19:01 -0700

    new method of lights

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 
 void setup() {
   Serial.begin(115200);
-
+  Serial.setTxTimeoutMs(0);
   for (int i = 0; i < NUM_BUTTONS; i++) {
     pinMode(buttonPins[i], INPUT_PULLUP);
   }
@@ -23,10 +23,20 @@ void setup() {
               1,            // Priority
               NULL          // Task handle
   );
+  xTaskCreate(writeTask,   // Function to run
+              "writeTask", // Name
+              100000,      // Stack size
+              NULL,        // Parameters
+              1,           // Priority
+              NULL         // Task handle
+  );
 }
 
 void loop() {
   // gamepadTask();
   keyboardTask();
+  // Serial.printf("Free heap: %d\n", ESP.getFreeHeap());
+  // Serial.printf("Minimum free heap: %d\n", ESP.getMinFreeHeap());
+  // Serial.printf("Maximum allocatable block: %d\n",
+  //               heap_caps_get_largest_free_block(MALLOC_CAP_DEFAULT));
 }
-
